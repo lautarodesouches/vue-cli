0a46985af4ca2165c5af654dcacc285ba8a4aba0
Lautaro Desouches <lautarodesouches@outlook.com>
2022-09-14 17:46:56 -0300

    Table & Title

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <h1 class="text-3xl font-bold underline bg-red-500">
-      Hello world!
-    </h1>
+  <div class='w-screen h-screen bg-gray-800 text-center flex flex-col items-center justify-center text-white'>
+    <CustomTitle title='Vue CLI' />
+    <CustomTable tableColor="green" :showBorder="true" :content="array1" />
+    <CustomTable tableColor="blue" :showBorder="true" :content="array2" />
+    <CustomTable tableColor="red" :showBorder="false" :content="array3" />
+  </div>
 </template>
-
+<!------------------------------------------------------------------------------------------->
 <script>
 
-  import HelloWorld from './components/HelloWorld.vue'
-  import '@/assets/tailwind.css'
+import CustomTable from './components/CustomTable.vue'
+import CustomTitle from './components/CustomTitle.vue'
+import '@/assets/tailwind.css'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CustomTable,
+    CustomTitle
+},
+  data() {
+    return {
+      array1: [
+        {
+          bg: 'bg-transparent',
+          list: ['Ciruela', '$143', '63']
+        },
+        {
+          bg: 'bg-transparent',
+          list: ['Manzana ', '$89', '43']
+        },
+        {
+          bg: 'bg-transparent',
+          list: ['Pera', '$54', '76']
+        }
+      ],
+      array2: [
+        {
+          bg: 'bg-transparent',
+          list: ['Apio', '$43', '123']
+        },
+        {
+          bg: 'bg-transparent',
+          list: ['Cebolla', '$25', '54']
+        },
+        {
+          bg: 'bg-transparent',
+          list: ['Esparrago', '$28', '79']
+        }
+      ],
+      array3: [
+        {
+          bg: 'bg-transparent',
+          list: ['Zapallo', '$43', '123']
+        },
+        {
+          bg: 'bg-transparent',
+          list: ['Calabaza', '$25', '54']
+        },
+        {
+          bg: 'bg-black',
+          list: ['Rabanito', '$28', '79']
+        }
+      ]
+    }
   }
 }
 </script>
-
+<!------------------------------------------------------------------------------------------->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/CustomTable.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class='my-4 w-1/4' >
+    <table class="table-auto rounded min-w-full" :class="'bg-'+ tableColor + '-500'">
+      <thead :class="showBorder && 'border-b border-white'">
+        <tr>
+          <th>Nombre</th>
+          <th>Precio</th>
+          <th>Cantidad</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in content" :class="item.bg" :key="item">
+          <td v-for="item in item.list" :key="item">
+            {{item}}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<!------------------------------------------------------------------------------------------->
+<script>
+export default {
+  name: 'CustomTable',
+  props: {
+    tableColor: String,
+    showBorder: Boolean,
+    content: Array
+  }
+}
+</script>
+<!------------------------------------------------------------------------------------------->
+<style scoped>
+
+</style>
